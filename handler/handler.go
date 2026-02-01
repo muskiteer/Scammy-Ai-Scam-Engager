617e16a1ad6d71fc88a177cba524b2a95edc9ca4
muskiteer <agarwal.vansh3110@gmail.com>
2026-02-02 02:22:54 +0530

    Done with the Bascis Now need the handlers

--- handler/handler.go
+++ handler/handler.go
@@ -2,7 +2,48 @@ package handler
 
 import (
 	"net/http"
+	"os"
 )
+
+type Request struct {
+	SessionID string `json:"sessionId"`
+	Message   MessageResponse `json:"message"`
+	ConvoHistory  []MessageResponse `json:"conversationHistory"`
+	Metadata    Metadata `json:"metadata"`
+}
+
+type MessageResponse struct {
+	Sender    string  `json:"sender"`
+	Text 	string  `json:"text"`
+	Timestamp string `json:"timestamp"`
+}
+
+type Metadata struct {
+	Channel string `json:"channel"`
+	Language string `json:"language"`
+	Locale string `json:"locale"`
+}
+
+type Response struct {
+	Status string `json:"status"`
+	Reply 	string `json:"reply"`
+}
+
+type ExtractedIntel struct {
+	UPIs   []string `json:"upiIds"`
+	Phones []string `json:"phoneNumbers"`
+	Links  []string `json:"phishingLinks"`
+	Banks  []string `json:"bankAccounts"`
+	Suswords []string `json:"suspiciousKeywords"`
+}
+
+type FinalResponse struct {
+	SessionID string `json:"sessionId"`
+	ScamDetect     bool `json:"scamDetected"`
+	TotalMessagesEx  int `json:"totalMessagesExchanged"`
+	ExtractIntel   ExtractedIntel   `json:"extractedIntelligence"` 	
+	AgentNote  string `json:"agentNotes"`	
+}
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -10,6 +51,11 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func StartConvo(w http.ResponseWriter, r *http.Request) {
+	API_KEY := os.Getenv("API_KEY")
+	if API_KEY == "" {
+		http.Error(w, "API key not set", http.StatusInternalServerError)
+		return
+	}
 	
 }
 	
--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import (
 	"log"
 
 	"github.com/muskiteer/Ai-Scam/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	routes.SetupRoutes(mux)
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found or error loading .env file")
+	}
 	
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
-	
+
 }	
